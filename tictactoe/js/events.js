d676a8ed73f17c1d20f81fcc88b4c4e135269e64
thebridge-tech <david.carvajal@thebridgeschool.es>
2021-02-16 11:23:00 +0100

    Events, callbacks & DOM

---
+++ tictactoe/js/events.js
@@ -0,0 +1,27 @@
+
+let currentToken = "X";
+
+// Capturar el elemento disparador....
+const pos00 = document.getElementById("position0_0");
+
+// Asignarle un callback ante un evento
+pos00.addEventListener("click", function () { // inline anonymous function
+
+  putToken(currentToken, this.id);
+});
+
+// Definir la función de callback
+function putToken(token, position) {
+  // Poner una ficha en una posición
+
+  document
+    .getElementById(position)
+    .innerHTML = token;
+
+  document
+    .getElementById(position)
+    .className = "casillaMarcada";
+
+  currentToken = currentToken === "X" ? "O" : "X";
+}
+
